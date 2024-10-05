06abefa9a4e2e99c6442100f2d4615e505ea5164
Jessan23 <jessicaanairayagrez@gmail.com>
2024-10-05 08:10:50 +0000

    corregir porque en vez de salir excusas salen numeros aleatorios

--- src/app.js
+++ src/app.js
@@ -4,8 +4,26 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+
+  let quien = [Math.floor(Math.random() * who.length)];
+  let accion = [Math.floor(Math.random() * action.length)];
+  let que = [Math.floor(Math.random() * what.length)];
+  let cuando = [
+    Math.floor(
+      Math.random() *
+        [
+          "before the class",
+          "when I was sleeping",
+          "while I was exercising",
+          "during my lunch",
+          "while I was praying"
+        ].length
+    )
+  ];
+  let excusaGenerada = [quien] + [accion] + [que] + [cuando];
+  document.getElementById("excuse").innerHTML = excusaGenerada;
 };
