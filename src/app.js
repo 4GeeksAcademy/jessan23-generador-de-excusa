30d4384f73e27d7cc78d83223716fc60125d05a3
Jessan23 <jessicaanairayagrez@gmail.com>
2024-10-07 02:41:19 +0000

    entrega pryecto generador de excusa

--- src/app.js
+++ src/app.js
@@ -4,26 +4,32 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+
 window.onload = function() {
   let who = ["The dog", "My grandma", "The mailman", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "my phone", "the car"];
-
-  let quien = [Math.floor(Math.random() * who.length)];
-  let accion = [Math.floor(Math.random() * action.length)];
-  let que = [Math.floor(Math.random() * what.length)];
-  let cuando = [
-    Math.floor(
-      Math.random() *
-        [
-          "before the class",
-          "when I was sleeping",
-          "while I was exercising",
-          "during my lunch",
-          "while I was praying"
-        ].length
-    )
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
   ];
-  let excusaGenerada = [quien] + [accion] + [que] + [cuando];
-  document.getElementById("excuse").innerHTML = excusaGenerada;
+
+  // Función para generar y mostrar la excusa
+  function generateExcuse() {
+    let quien = Math.floor(Math.random() * who.length);
+    let accion = Math.floor(Math.random() * action.length);
+    let que = Math.floor(Math.random() * what.length);
+    let cuando = Math.floor(Math.random() * when.length);
+
+    let excusaGenerada = `${who[quien]} ${action[accion]} ${what[que]} ${when[cuando]}`;
+
+    // Mostrar la excusa
+    document.getElementById("excuse").innerHTML = excusaGenerada;
+  }
+
+  // Asignar evento al botón
+  document.getElementById("generate").addEventListener("click", generateExcuse);
 };
