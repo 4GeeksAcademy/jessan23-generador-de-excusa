e8af393fcb353612d61d5abf2951ed62cb7b1e05
Jessan23 <jessicaanairayagrez@gmail.com>
2024-10-17 03:14:20 +0000

    quite comentarios que tenia

--- src/app.js
+++ src/app.js
@@ -2,9 +2,6 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   let who = ["The dog", "My grandma", "The mailman", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
@@ -17,7 +14,6 @@ window.onload = function() {
     "while I was praying"
   ];
 
-  // Función para generar y mostrar la excusa
   function generateExcuse() {
     let quien = Math.floor(Math.random() * who.length);
     let accion = Math.floor(Math.random() * action.length);
@@ -25,11 +21,7 @@ window.onload = function() {
     let cuando = Math.floor(Math.random() * when.length);
 
     let excusaGenerada = `${who[quien]} ${action[accion]} ${what[que]} ${when[cuando]}`;
-
-    // Mostrar la excusa
     document.getElementById("excuse").innerHTML = excusaGenerada;
   }
-
-  // Asignar evento al botón
   document.getElementById("generate").addEventListener("click", generateExcuse);
 };
